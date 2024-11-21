1dbd63dbaab2eb77766d39deb9933ddb994349d4
Bruno <111437933+brunotemote@users.noreply.github.com>
2024-11-21 19:51:39 -0300

    Update Agendavel.java

--- EscolaMusica/src/br/com/musica/interfaces/Agendavel.java
+++ EscolaMusica/src/br/com/musica/interfaces/Agendavel.java
@@ -1,5 +1,5 @@
 package br.com.musica.interfaces;
-
+//interface define o contrato para agendamento e cancelamento de aulas
 public interface Agendavel {
     void agendarAula(String aula);
     void cancelarAula(String aula);
