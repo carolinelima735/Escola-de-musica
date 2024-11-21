59ad7369df75485bb1345c393ce262118c1c9aa4
Bruno <111437933+brunotemote@users.noreply.github.com>
2024-11-21 19:55:32 -0300

    Update Main.java

--- EscolaMusica/src/br/com/musica/main/Main.java
+++ EscolaMusica/src/br/com/musica/main/Main.java
@@ -5,20 +5,20 @@ import br.com.musica.escola.*;
 
 public class Main {
     public static void main(String[] args) {
-        GerenciadorDeAulas gerenciador = GerenciadorDeAulas.getInstancia();
+        GerenciadorDeAulas gerenciador = GerenciadorDeAulas.getInstancia(); //Obtém a única instância do Gerenciador de Aulas (Singleton)
 
         Aluno aluno1 = new Aluno("Caroline", 21, "Taguatinga", "Piano");
         Professor professor1 = new Professor("bruno", 20, "Aguas Claras", "Violão");
-
+//adiciona aluno e professor como observadores do Gerenciador de Aulas (Observer)
         gerenciador.adicionarObservador(aluno1);
         gerenciador.adicionarObservador(professor1);
-
+//notifica os observadores sobre agendamento e cancelamentos das aulas 
         gerenciador.agendarAula("Aula de Piano - 10:00");
         gerenciador.agendarAula("Aula de Violão - 14:00");
         gerenciador.cancelarAula("Aula de Piano - 10:00");
-
+//exibe aulas agendadas
         gerenciador.listarAulas();
-
+// simula inscricao e cancelamento por parte do aluno
         aluno1.inscreverCurso("Violão");
         aluno1.cancelarInscricao();
     }
