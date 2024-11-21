f13c6fd4837f8c90687f70dc76c4c1d6cc586c9c
Bruno <111437933+brunotemote@users.noreply.github.com>
2024-11-21 18:52:41 -0300

    Update CursoDeViolao.java

--- EscolaMusica/src/br/com/musica/escola/CursoDeViolao.java
+++ EscolaMusica/src/br/com/musica/escola/CursoDeViolao.java
@@ -1,5 +1,5 @@
 package br.com.musica.escola;
-
+// construtor que inicia o nome do curso 
 public class CursoDeViolao extends Curso {
     public CursoDeViolao() {
         super("Curso de Violão");
