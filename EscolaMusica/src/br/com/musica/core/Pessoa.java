068a7bc3a240744c63ed29352c3866eded54e10e
Bruno <111437933+brunotemote@users.noreply.github.com>
2024-11-21 18:43:36 -0300

    Update Pessoa.java

--- EscolaMusica/src/br/com/musica/core/Pessoa.java
+++ EscolaMusica/src/br/com/musica/core/Pessoa.java
@@ -1,5 +1,8 @@
 package br.com.musica.core;
+ //abstracao: classe base que nao sera instanciada diretamente servindo como modelo para classes derivadas (aluno professor)
+// encapsulamento: os atributos sao privados e acessados apenas por métodos publicos
 
+//construtor da classe pessoa
 public abstract class Pessoa {
     private String nome;
     private int idade;
