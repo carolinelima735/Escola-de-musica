60889850e44d896cb447dcb9ef8d1c8ac8e3e36e
Bruno <111437933+brunotemote@users.noreply.github.com>
2024-11-21 18:40:11 -0300

    Update Aluno.java

--- EscolaMusica/src/br/com/musica/core/Aluno.java
+++ EscolaMusica/src/br/com/musica/core/Aluno.java
@@ -5,16 +5,17 @@ import br.com.musica.escola.Observador;
 public class Aluno extends Pessoa implements Observador {
     private String curso;
 
+    //construtor classe aluno 
     public Aluno(String nome, int idade, String endereco, String curso) {
         super(nome, idade, endereco);
         this.curso = curso;
     }
-
+// inscreve o aluno no curso
     public void inscreverCurso(String curso) {
         this.curso = curso;
         System.out.println(getNome() + " foi inscrito no curso: " + curso);
     }
-
+// cancela inscricao
     public void cancelarInscricao() {
         System.out.println(getNome() + " cancelou a inscrição no curso: " + curso);
         this.curso = null;
@@ -23,5 +24,6 @@ public class Aluno extends Pessoa implements Observador {
     @Override
     public void atualizar(String mensagem) {
         System.out.println("Aluno " + getNome() + " foi notificado: " + mensagem);
+        // exibe informacoes sobre o aluno 
     }
 }
