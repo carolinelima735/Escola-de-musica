6966d2c06c2d6d346d6d06e77162df389561732f
Bruno <111437933+brunotemote@users.noreply.github.com>
2024-11-21 18:54:18 -0300

    Update CursoFactory.java

--- EscolaMusica/src/br/com/musica/escola/CursoFactory.java
+++ EscolaMusica/src/br/com/musica/escola/CursoFactory.java
@@ -1,6 +1,7 @@
 package br.com.musica.escola;
-
+//factory responsavel por criar instancias de diferentes tipos de cursos.
 public class CursoFactory {
+    //cria uma instancia de curso com base no tipo especificado
     public static Curso criarCurso(String tipo) {
         if (tipo == null || tipo.isEmpty()) {
             throw new IllegalArgumentException("Tipo de curso inválido!");
