9d376f8dcac00c5fb00f3026c3ac350c684308db
Bruno <111437933+brunotemote@users.noreply.github.com>
2024-11-21 18:51:12 -0300

    Update Curso.java

--- EscolaMusica/src/br/com/musica/escola/Curso.java
+++ EscolaMusica/src/br/com/musica/escola/Curso.java
@@ -1,13 +1,15 @@
 package br.com.musica.escola;
+// abstracao: serve como modelo para especializacoes como CursoDePiano ou CursoDeViolao.
+ // - encapsulamento: atributo privado "nome" acessado e modificado por metodos publicos.
 
 //Classe base para os cursos
 public abstract class Curso {
  private String nome;
-
+//retorna ao nome do curso
  public Curso(String nome) {
      this.nome = nome;
  }
-
+//define o nome do curso
  public String getNome() {
      return nome;
  }
@@ -15,7 +17,7 @@ public abstract class Curso {
  public void setNome(String nome) {
      this.nome = nome;
  }
-
+//exibe detalhes sobre o curso
  public void detalhes() {
      System.out.println("Curso: " + nome);
  }
